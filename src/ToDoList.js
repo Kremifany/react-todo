829688caa1edba372086e125509f83bbb785cfba
Kremifany <34751960+Kremifany@users.noreply.github.com>
2023-06-11 22:48:33 -0400

    <input

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,16 +1,11 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const todoList = [];
-
-todoList[0] = { id: "1", title: "Complete assignment" };
-todoList[1] = { id: "2", title: "Upload to Github" };
-todoList[2] = { id: "3", title: "Upload the link" };
-
-function TodoList() {
+function TodoList({ todoList }) {
   return (
     <ul>
       {todoList.map((item) => {
+        console.log(item.id, item.title);
         return <TodoListItem key={item.id} todo={item} />;
       })}
     </ul>
